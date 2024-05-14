d08619f99008db362626931abfdee6256daa8e9e
tinyheros <tinyherosgame@gmail.com>
2024-05-14 13:15:32 -0700

    update

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
 export default async function handler(req, res) {
-    const { body } = req;
-    return res.send(`Hello ${body.name}, you just parsed the request body!`);
+    return res.send({message: "Solana API"});
   }
