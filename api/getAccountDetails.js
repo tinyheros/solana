3c237da4d1629a60adc92593d8d4caf5fe045dec
tinyheros <tinyherosgame@gmail.com>
2024-05-15 15:03:50 -0700

    update

--- api/getAccountDetails.js
+++ api/getAccountDetails.js
@@ -8,7 +8,7 @@ export default cors(async function handler(req, res) {
         const lamports = await connection.getBalance(new PublicKey(req.body.address));
         const solBalance = lamports / Math.pow(10, 9);
         res.status(200).json({
-            balance: solBalance,
-            address: req.body.address
+            sol_balance: solBalance,
+            default_address: req.body.address
         })
 })
