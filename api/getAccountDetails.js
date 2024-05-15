45eadcd924be86761a8fad07c54c3d2b897e0e5f
tinyheros <tinyherosgame@gmail.com>
2024-05-15 02:24:27 -0700

    update

--- api/getAccountDetails.js
+++ api/getAccountDetails.js
@@ -6,5 +6,5 @@ export default async function handler(req, res) {
     const lamports = await connection.getBalance(new PublicKey("2vU4z8MBhekmvR2LRrJ3ZKNZRXZtwHvuNe2esEG44YCE"));
     const solBalance = lamports / Math.pow(10, 9);
     console.log(solBalance)
-    res.json({address: req.body.address, solBalance})
+    res.json({solBalance})
 }
