80ceae42eeaf700656edd501ba2a768b2881a5e0
tinyheros <tinyherosgame@gmail.com>
2024-05-15 12:19:47 -0700

    update

--- api/getAccountDetails.js
+++ api/getAccountDetails.js
@@ -3,7 +3,7 @@ import { Connection, PublicKey } from '@solana/web3.js'
 
 export default async function handler(req, res) {
     const connection = new Connection('https://api.mainnet-beta.solana.com');
-    const lamports = await connection.getBalance(new PublicKey("2vU4z8MBhekmvR2LRrJ3ZKNZRXZtwHvuNe2esEG44YCE"));
+    const lamports = await connection.getBalance(new PublicKey(res.body.address));
     const solBalance = lamports / Math.pow(10, 9);
     console.log(solBalance)
     res.json({solBalance})
--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 export default async function handler(req, res) {
-    return res.send({message: "Solana API"});
+    return res.json({message: "Solana API"});
   }
