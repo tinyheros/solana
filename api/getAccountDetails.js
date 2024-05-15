7aa434cc2f3ce1055ca7e34dcfe29609a28e3ef5
tinyheros <tinyherosgame@gmail.com>
2024-05-15 15:11:16 -0700

    update

--- api/getAccountDetails.js
+++ api/getAccountDetails.js
@@ -15,6 +15,8 @@ export default cors(async function handler(req, res) {
         return res.status(400).json({ error: 'Address is required' });
     }
 
+    const tokenProgramId = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'; // Use the provided token program ID
+
     try {
         const connection = new Connection('https://api.mainnet-beta.solana.com');
 
@@ -23,8 +25,7 @@ export default cors(async function handler(req, res) {
         const solBalance = solLamports / Math.pow(10, 9);
 
         // Get SPL Token balance
-        const tokenProgramId = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
-        const token = new Token(connection, tokenProgramId, Token.getLayout('single'));
+        const token = new Token(connection, new PublicKey(tokenProgramId), null, []);
         const tokenAccount = await token.getAccountInfo(new PublicKey(address));
         const tokenBalance = tokenAccount ? tokenAccount.amount.toNumber() / Math.pow(10, 6) : 0;
 
