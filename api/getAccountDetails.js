0087775d3e822bd277501265b97f10a49158519f
tinyheros <tinyherosgame@gmail.com>
2024-05-15 16:35:00 -0700

    update

--- api/getAccountDetails.js
+++ api/getAccountDetails.js
@@ -1,14 +1,34 @@
-import { Connection, PublicKey } from '@solana/web3.js'
+import { Connection, PublicKey } from '@solana/web3.js';
+import { getAssociatedTokenAddress } from '@solana/spl-token';
 import Cors from 'micro-cors';
 
 const cors = Cors();
 
 export default cors(async function handler(req, res) {
-        const connection = new Connection('https://api.mainnet-beta.solana.com');
-        const lamports = await connection.getBalance(new PublicKey(req.body.address));
-        const solBalance = lamports / Math.pow(10, 9);
-        res.status(200).json({
-            sol_balance: solBalance,
-            default_address: req.body.address
-        })
-})
+    const connection = new Connection('https://api.mainnet-beta.solana.com');
+    const walletAddress = new PublicKey(req.body.address);
+
+    // Fetching SOL balance
+    const lamports = await connection.getBalance(walletAddress);
+    const solBalance = lamports / Math.pow(10, 9);
+
+    // Address of the SPL token
+    const tokenMintAddress = new PublicKey('D3u7mgroPcbTm62GabAZSLhDS98ybHD9APJLARcdTjXz');
+
+    // Finding the associated token account
+    const tokenAccountAddress = await getAssociatedTokenAddress(
+        tokenMintAddress,
+        walletAddress
+    );
+
+    // Fetching SPL token balance
+    const tokenAccountInfo = await connection.getParsedAccountInfo(tokenAccountAddress);
+    const tinyBalance = tokenAccountInfo.value.data.parsed.info.tokenAmount.uiAmount;
+
+    // Respond with JSON data
+    res.status(200).json({
+        sol_balance: solBalance,
+        tiny_balance: tinyBalance,
+        default_address: req.body.address
+    });
+});
