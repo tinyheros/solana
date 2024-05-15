83c99d499e924f26ea258153df2de20f3ec1d4c3
tinyheros <tinyherosgame@gmail.com>
2024-05-15 14:45:32 -0700

    update

--- api/getAccountDetails.js
+++ api/getAccountDetails.js
@@ -6,7 +6,7 @@ const cors = Cors();
 export default cors(async function handler(req, res) {
     console.log(req.body.address)
         const connection = new Connection('https://api.mainnet-beta.solana.com');
-        const lamports = await connection.getBalance(new PublicKey(res.body.address));
+        const lamports = await connection.getBalance(new PublicKey(req.body.address));
         const solBalance = lamports / Math.pow(10, 9);
         res.status(200).json({
             balance: solBalance,
