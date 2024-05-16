8bcc6da519d0e834788315cb16c26dc3cb5161e2
tinyheros <tinyherosgame@gmail.com>
2024-05-16 00:15:58 -0700

    update

--- api/getAccountDetails.js
+++ api/getAccountDetails.js
@@ -11,28 +11,16 @@ const creatorAddress = '2vU4z8MBhekmvR2LRrJ3ZKNZRXZtwHvuNe2esEG44YCE';
 
 export default cors(async function handler(req, res) {
     const walletAddress = new PublicKey(req.body.address);
-
-    // Fetching SOL balance
     const lamports = await connection.getBalance(walletAddress);
     const solBalance = lamports / Math.pow(10, 9);
-
-    // Address of the SPL token
     const tokenMintAddress = new PublicKey('D3u7mgroPcbTm62GabAZSLhDS98ybHD9APJLARcdTjXz');
-
-    // Finding the associated token account
     const tokenAccountAddress = await getAssociatedTokenAddress(
         tokenMintAddress,
         walletAddress
     );
-
-    // Fetching SPL token balance
     const tokenAccountInfo = await connection.getParsedAccountInfo(tokenAccountAddress);
     const tinyBalance = tokenAccountInfo.value.data.parsed.info.tokenAmount.uiAmount;
-
-    // Fetching NFT metadata
     const nftCollection = await findNFTsCreatedBy(walletAddress.toString());
-
-    // Respond with JSON data
     res.status(200).json({
         sol_balance: solBalance,
         tiny_balance: tinyBalance,
@@ -62,12 +50,8 @@ async function findNFTsCreatedBy(walletAddress) {
 
                 if (metadata.data.data.creators.some(creator => creator.address === creatorPublicKey.toString())) {
                     const uri = metadata.data.data.uri;
-
-                    // Fetching the URI metadata
                     const response = await fetch(uri);
                     const uriMetadata = await response.json();
-
-                    // Add the URI metadata to the collection
                     nftCollection.push({
                         mint: tokenMintAddress.toString(),
                         uriMetadata
